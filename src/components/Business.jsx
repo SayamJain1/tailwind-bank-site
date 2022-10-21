506baa2c5b647ff55c1dac821bcede28eacb839c
sanyam jain <sayamjain99300@gmail.com>
2022-10-21 08:54:48 +0530

    initial commit

---
+++ src/components/Business.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from './Button'
+
+const FeatureCard = ({icon, title, content, i}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${i !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon}  className='w-[50%] h-[50%] object-contain'/>
+    </div>
+
+    <div className='flex-1 flex flex-col ml-6'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>{title}</h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>{content}</p>
+    </div>
+  </div>
+)
+
+const Business = () => {
+  return (
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className='sm:block hidden' /> we'll handle the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla dolor repudiandae iste nostrum beatae sed error eaque tempore minus voluptatem.</p>
+
+        <Button styles='mt-10' />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, i) => (
+          <FeatureCard key={feature.id} {...feature} i={i} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Business
